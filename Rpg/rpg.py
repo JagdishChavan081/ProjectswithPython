b5653dc626f0b1af5fe19a53ff00cbf96e69da16
JAGDISH CHAVAN <54933866+JagdishChavan081@users.noreply.github.com>
2022-02-04 11:15:48 +0530

    Add files via upload

---
+++ Rpg/rpg.py
@@ -0,0 +1,13 @@
+import string
+import random
+
+def randompassword():
+    chars = string.ascii_letters + string.digits
+    size = 3 
+    return "".join(random.choice(chars) for x in range(size, 10))
+
+
+n=0
+while n <10:
+    print(randompassword())
+    n=n+1
